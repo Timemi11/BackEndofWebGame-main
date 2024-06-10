5e5836f8ddc651359bad15d7e783d8ab578f7fb4
time <chatchawan.aromtip@gmail.com>
2024-06-10 21:52:40 +0700

    ssss

--- dist/controller/line.controller.js
+++ dist/controller/line.controller.js
@@ -16,18 +16,35 @@ class LineController {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
             const body = (_a = req.body.events[0]) !== null && _a !== void 0 ? _a : undefined;
+            console.log(body);
             if (!body)
                 return res.sendStatus(200).end();
-            const result = yield line_service_1.LineService.sendWebhook(body);
-            return res.status(200).json(result); //optional return
+            try {
+                const result = yield line_service_1.LineService.sendWebhook(body);
+                return res.status(200).send(result);
+            }
+            catch (error) {
+                console.error("Error in sendWebhook:", error);
+                return res.status(500).send("Error in Webhook");
+            }
         });
     }
     static sendMessageToLine(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const userId = req.params.userId;
+            console.log(userId);
             const body = req.body;
-            const result = yield line_service_1.LineService.sendMessageToLine(userId, body);
-            return res.status(200).json(result); //optional return
+            console.log(body);
+            try {
+                const result = yield line_service_1.LineService.sendMessageToLine(userId, body);
+                return res
+                    .status(200)
+                    .json({ message: "Message sent successfully", result });
+            }
+            catch (error) {
+                console.error("Error in sendMessageToLine:", error);
+                return res.status(500).json({ error: "Failed to send message" });
+            }
         });
     }
 }
