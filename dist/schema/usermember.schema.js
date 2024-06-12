b9cf6f9780d42f78d120e7c3eab156caa0851bdf
Timemi11 <momuu56@gmail.com>
2024-06-12 14:18:56 +0700

    appId

--- dist/schema/usermember.schema.js
+++ dist/schema/usermember.schema.js
@@ -1,12 +1,19 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserMemberSchema = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
-exports.UserMemberSchema = new mongoose_1.default.Schema({
+const mongoose_1 = require("mongoose");
+const PriceSchema = new mongoose_1.Schema({
+    formattedPrice: String,
+});
+exports.UserMemberSchema = new mongoose_1.Schema({
     userId: String,
     displayName: String,
-    wishList: [{ gameId: Number }]
+    wishList: [
+        {
+            name: String,
+            appId: Number,
+            image: String,
+            price: PriceSchema,
+        },
+    ],
 });
