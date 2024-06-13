ec5c369e72442c1b89e38882e9c02bcbf7634a27
Timemi11 <momuu56@gmail.com>
2024-06-13 13:18:51 +0700

    build fix flex

--- dist/services/line.service.js
+++ dist/services/line.service.js
@@ -51,79 +51,30 @@ class LineService {
             const appList = app === null || app === void 0 ? void 0 : app.wishList;
             const wishListText = (appList === null || appList === void 0 ? void 0 : appList.map((item) => item.name).join(" ")) || "";
             // const steamUrlGame = "https://store.steampowered.com/app/$appId";
-            // type: "flex",
-            // altText: "รายการโปรดของคุณ",
-            // contents: {
-            //     type: "bubble",
-            //     size: "giga",
-            //     body: {
-            //       type: "box",
-            //       layout: "vertical",
-            //       contents: [
-            //         {
-            //           type: "text",
-            //           text: "รายการโปรด",
-            //           weight: "bold",
-            //           size: "xxl",
-            //         },
-            //       ],
-            //     },
-            //     footer: {
-            //       type: "box",
-            //       layout: "vertical",
-            //       spacing: "sm",
-            //       contents: [
-            //         {
-            //           type: "box",
-            //           layout: "horizontal",
-            //           contents: [
-            //             {
-            //               type: "text",
-            //               text: item.name, // แสดงชื่อเกม
-            //               align: "start",
-            //               flex: 4,
-            //             },
-            //             {
-            //               type: "text",
-            //               text: "เลือกดู",
-            //               align: "end",
-            //               action: {
-            //                 type: "uri",
-            //                 label: "action",
-            //                 uri: `https://store.steampowered.com/app/${item.appId}`, // ลิงก์เป็นลิงก์ของเกม
-            //               },
-            //             },
-            //           ],
-            //           justifyContent: "flex-start",
-            //           alignItems: "flex-start",
-            //         },
-            //       ],
-            //       flex: 0,
-            //       alignItems: "flex-start",
-            //       justifyContent: "center",
-            //     },
-            // },
-            const flexContents = (appList === null || appList === void 0 ? void 0 : appList.map((item) => ({
-                type: "bubble",
-                size: "giga",
-                body: {
-                    type: "box",
-                    layout: "vertical",
-                    contents: [
-                        {
-                            type: "text",
-                            text: "รายการโปรด",
-                            weight: "bold",
-                            size: "xxl",
-                        },
-                    ],
-                },
-                footer: {
-                    type: "box",
-                    layout: "vertical",
-                    spacing: "sm",
-                    contents: [
-                        {
+            const flexMessage = {
+                type: "flex",
+                altText: "รายการโปรดของคุณ",
+                contents: {
+                    type: "bubble",
+                    size: "giga",
+                    body: {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                            {
+                                type: "text",
+                                text: "รายการโปรด",
+                                weight: "bold",
+                                size: "xxl",
+                            },
+                            // Add more contents if needed within the body
+                        ],
+                    },
+                    footer: {
+                        type: "box",
+                        layout: "vertical",
+                        spacing: "sm",
+                        contents: appList === null || appList === void 0 ? void 0 : appList.map((item) => ({
                             type: "box",
                             layout: "horizontal",
                             contents: [
@@ -146,17 +97,12 @@ class LineService {
                             ],
                             justifyContent: "flex-start",
                             alignItems: "flex-start",
-                        },
-                    ],
-                    flex: 0,
-                    alignItems: "flex-start",
-                    justifyContent: "center",
+                        })),
+                        flex: 0,
+                        alignItems: "flex-start",
+                        justifyContent: "center",
+                    },
                 },
-            }))) || [];
-            const flexMessage = {
-                type: "flex",
-                altText: "รายการโปรดของคุณ",
-                contents: flexContents,
             };
             if (event.type === "message") {
                 const message = event.message;
@@ -223,7 +169,7 @@ class LineService {
                     else if (message.text === "รายการโปรด") {
                         client.replyMessage({
                             replyToken: event.replyToken,
-                            messages: [flexMessage],
+                            messages: flexMessage,
                         });
                     }
                     else {
