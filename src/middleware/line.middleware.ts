def71a580894a5e1864f207e78671965a6400947
Timemi11 <momuu56@gmail.com>
2024-06-28 14:23:16 +0700

    check log

--- src/middleware/line.middleware.ts
+++ src/middleware/line.middleware.ts
@@ -3,12 +3,21 @@ import { getUserProfile, verifyToken } from "../util/line"
 
 export const lineMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const authentication = req.headers["authorization"]
+    console.log("authentication")
+    console.log(authentication)
+
+
     if (!authentication) {
+        console.log("!authentication")
+        console.log(authentication)
         return res.status(401).json({ message: "Unauthorized" })
     }
     const [type, token] = authentication.split(" ")
 
     if (type !== "Bearer" || !token) {
+        console.log("!Bearer")
+        console.log(type)
+        console.log(token)
         return res.status(401).json({ message: "Unauthorized" })
     }
 
